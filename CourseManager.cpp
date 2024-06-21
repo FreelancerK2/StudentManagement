6060e5014be2cdd2ccdbcbe985f2d9336d35ba6c
FreelancerK2 <lensomnang.ls@gmail.com>
2024-06-21 22:03:45 +0700

    file building

---
+++ CourseManager.cpp
@@ -0,0 +1,174 @@
+#include <fstream>
+#include "Course.cpp"
+#include "CourseRepository.cpp"
+#include "Programming.cpp"
+#include "Design.cpp"
+#include "Protect.cpp "
+using namespace std;  
+
+class CourseManager : public CourseRepository {
+private:
+    std::vector<Course*> courses;
+    string filename;
+    // Find ID by each course
+    vector<Course*>::iterator findCourseById(int id) {
+        return std::find_if(courses.begin(), courses.end(), [id](Course* course) {
+            return course->getCourseID() == id;
+        });
+    }
+    // Check over Duplicate ID
+    bool isDuplicateId(int id) const {
+        return any_of(courses.begin(), courses.end(), [id](Course* course) { return course->getCourseID() == id; });
+    }
+ 
+public:
+       CourseManager(const std::string& filename) : filename(filename) {
+    }
+
+    // Function to add more course
+    void addCourse(Course* course) {
+        if (isDuplicateId(course->getCourseID())) {
+            cout << "Course with ID " << course->getCourseID() << " already exists. Cannot add duplicate." << endl;
+            delete course; // Prevent memory leak
+        } else {
+            courses.push_back(course);
+            cout << "\nCourse added successfully." << endl;
+        }
+    }
+
+    // Function to Display all course
+    void viewCourses() override {                                                                                                
+        cout <<"No."<< setw(11) << "Name" << setw(19) << "CourseID" << setw(20) << "Duration(Years)"<< setw(15) <<"Type" << setw(28) << "Project/Tools"<< endl;
+        cout << string(97, '-') << endl;
+        // Check for valid input
+        if (courses.empty()) {
+            throw std::invalid_argument("No courses available");
+            return;
+        } 
+        int i = 1;
+        std::for_each(courses.begin(), courses.end(), [&i](Course* course) {
+        cout <<i++;
+        course->displayInfo();
+        }); 
+    }
+    // Function to update course
+    void updateCourse(int id) {
+    auto it = findCourseById(id);
+        if (it != courses.end()) {
+            std::cout << "Which item do you want to update?\n"
+                      << "1. Name\n2. Duration\n3. CourseID\n";
+            Programming* program = dynamic_cast<Programming*>(*it);
+            Design* design = dynamic_cast<Design*>(*it);
+            if (program) {            
+                std::cout << "4. Project\n\nEnter your choice: ";
+            } 
+            else if (design) {   
+                std::cout << "4. Tools\n\nEnter your choice:";
+            }
+            int choice;
+            std::cin >> choice;
+            std::cin.ignore();
+
+            switch (choice) {
+                case 1: {
+                    std::string newName = Protect::getStringInput("Enter new name: ");
+                    (*it)->setName(newName);
+                    break;
+                } 
+                case 2: {
+                    double newDuration = Protect::getDoubleInput("Enter new duration: ");
+                     (*it)->setDuration(newDuration);
+                    break;
+                }
+                case 3: {
+                    int newId = Protect::getIntInput("Enter new ID: ");
+                    Protect::validateId(id);
+                    (*it)->setId(newId);
+                    break;
+                }
+                case 4: {
+                    if (program) {   
+                        std::string newProject = Protect::getStringInput("Enter new Project: ");
+                        std::cin.ignore();
+                        program->setProject(newProject);
+                    } else if (design) {      
+                        std::string newTools = Protect::getStringInput("Enter new Tools: ");
+                        std::cin.ignore();
+                        design->setTools(newTools);
+                    }
+                    break;
+                }
+                default:
+                    throw std::invalid_argument("Invalid input.");
+            }
+        } else {
+            throw std::invalid_argument("Course not found.\n");
+        }
+    }
+    // Function Delete Course
+    void deleteCourse(int id) override {
+        auto it = std::remove_if(courses.begin(), courses.end(), [id](Course* course) {
+            return course->getCourseID() == id;
+        });
+        // Execute if encounter ID
+        if (it != courses.end()) {
+            courses.erase(it, courses.end());
+            std::cout << "\nCourse deleted successfully.\n";
+        } else {
+            throw std::invalid_argument("Course not found.\n");
+        }
+    }
+    // Function write to file
+    void saveToFile(const std::string &filename) const {
+        std::ofstream outFile(filename, std::ios::binary);
+        for (const auto &course : courses) {
+            course->save(outFile);
+        }
+        cout <<"\nData saved successfully"<<endl;
+        outFile.close();
+    }
+    // Function read from file
+    void loadFromFile(const string& filename) {
+        ifstream file(filename, ios::binary);
+        if (file.is_open()) {
+            while (!file.eof()) {
+                string type;
+                size_t typelength;
+                file.read(reinterpret_cast<char*>(&typelength), sizeof(typelength));
+                type.resize(typelength);
+                file.read(&type[0], typelength);
+                Course*course = nullptr;
+                if (type == "Programming") {
+                    course = new Programming();
+                    course->load(file);
+                    courses.push_back(course);
+                } else if (type == "Design") {
+                    course = new Design();
+                    course->load(file);
+                    courses.push_back(course);
+                }
+                cout <<"\nData loaded successfully"<<endl;
+            }
+            file.close();
+        } else {
+            cout << "Unable to open file for reading!" << endl;
+        }
+    }
+
+    // destroy pointer
+    ~CourseManager() {
+        for (auto course : courses) {
+            delete course;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
---
+++ Protect.cpp
@@ -0,0 +1,155 @@
+#pragma once
+#include "Course.cpp"
+#include "Programming.cpp"
+#include "Design.cpp"
+using namespace std;
+
+class Protect {
+public:
+
+    // Check if ID is less than Zero or ID in letter
+    static void validateId(int id) {
+        if (cin.fail() || id <= 0) {
+            throw std::invalid_argument("ID must be a positive integer.");
+        }
+    }
+
+    // Function to input integer
+    static int getIntInput(const std::string& prompt) {
+        int input;
+        std::cout << prompt;
+        while (!(std::cin >> input)) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input."<<endl;
+            cout <<prompt;
+        } 
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return input;
+    }  
+
+    // Function to input String
+    static std::string getStringInput(const std::string& prompt) {
+        std::string input;
+        std::cout << prompt;
+        cin >> input;
+
+        std::regex numberPattern("[0-9]"); // Regex to match any digit
+        while (input.length() <= 1 || std::regex_search(input, numberPattern)) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input. "<<endl;
+            cout <<prompt;
+            cin >> input;
+            // Check if the input contains any numbers
+            if (std::regex_search(input, numberPattern)) {
+                std::cout << "The string should not contain numbers! ";
+            } else if(input.length() <= 1){
+                std::cout <<"The string length not less than one or equal once! ";
+            }else{
+                break;
+            } 
+        }
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return input;
+    }
+
+    // Ask user to repeat action 
+    static bool askToAddMore() {
+        char choice;
+        while (true) {
+            std::cout << "\nDo you want to make more (y/n)? ";
+            std::cin >> choice;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear the input buffer
+
+            // Convert choice to lowercase to handle 'Y' and 'N'
+            choice = std::tolower(static_cast<unsigned char>(choice));
+
+            if (choice == 'y') {
+                return true;
+            } else if (choice == 'n') {
+                return false;
+            } else {
+                std::cout << "Invalid input! Please enter 'y' or 'n'." << std::endl;
+            }
+        }
+    }
+
+    // Function to input double
+    static double getDoubleInput(const std::string& prompt) {
+        double value;
+        cout <<prompt;
+        while (!(std::cin >> value)) {
+            std::cin.clear(); // Clear the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore invalid input
+            std::cout << "Invalid input.Please input only Number!"<<endl;
+            cout<<prompt;
+        } 
+        return value;
+    }
+
+    // Make option of Programming Course
+    static string inputProgram(string program){
+        int choice;
+        do{
+            cout <<program<<endl;
+            cout <<"1. Software Development."<<endl;
+            cout <<"2. Language Python."<<endl;
+            cout <<"3. Web Development."<<endl;
+            cout <<"4. IT Support."<<endl;
+            cout <<"5. WordPress Website Development."<<endl;
+            cout <<"0. Exit"<<endl;
+            cout <<"\nEnter your choice<0-5>: ";
+            cin >> choice;
+            switch(choice){
+                case 1 : return "Software Development.";
+                        break;
+                case 2 : return "Language Python.";
+                        break;
+                case 3 : return "Web Development.";
+                        break;
+                case 4 : return "IT Support.";
+                        break;
+                case 5 : return "WordPress WebDevelopment.";
+                        break;
+                case 0 : return "Exit";
+                        break;
+                default: return "Invalid Input";
+                        break;
+            }
+        }while(choice != 0);
+    }
+
+    //Make option of Design Course
+    static string inputDesignCourse(string design){
+        int choice;
+        do{
+            cout <<design<<endl;
+            cout <<"\t1. Graphic and Design."<<endl;
+            cout <<"\t2. Adobe Photoshop."<<endl;
+            cout <<"\t3. Video Editing."<<endl;
+            cout <<"\t4. 3D Motion."<<endl;
+            cout <<"\t5. UX/UI Design."<<endl;
+            cout <<"\t0. Exit"<<endl;
+            cout <<"\tEnter your choice<0-5>: ";
+            cin >> choice;
+            switch(choice){
+                case 1 : return "Graphic and Design.";
+                        break;
+                case 2 : return "Adobe Photoshop.";
+                        break;
+                case 3 : return "Video Editing.";
+                        break;
+                case 4 : return "3D Motion.";
+                        break;
+                case 5 : return "UX/UI Design.";
+                        break;
+                case 0 : return "Exit";
+                        break;
+                default: return "Invalid Input";
+                        break;
+            }
+        }while(choice != 0);
+    }
+
+};
